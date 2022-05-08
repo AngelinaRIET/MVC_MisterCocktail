3645966663954f5f56cdd89ce1e376b02e195958
Angélina RIET <51684465+AngelinaRIET@users.noreply.github.com>
2022-05-08 14:27:34 +0200

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,8 @@
+<nav>
+    <a href="index.php">Accueil</a>
+    <a href="ajouter_cocktail.php">Ajouter un Cocktail</a>
+    <a href="backoffice.php">Backoffice Cocktail</a>
+    <a href="ajouter_ingredient.php">Ajouter un ingredient</a>
+    <a href="backoffice_ingredient.php">Backoffice ingredient</a>
+    <h1>Mister Cocktail</h1>
+</nav>
