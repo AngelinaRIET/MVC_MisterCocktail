02a74ade9834e6756c4bd1e0dc2b09a38afeebb4
Angélina RIET <51684465+AngelinaRIET@users.noreply.github.com>
2022-05-08 16:27:05 +0200

    Update edition_cocktail.php

--- edition_cocktail.php
+++ edition_cocktail.php
@@ -1,15 +1,9 @@
 <?php
 
-    require_once 'lib/database.php';
-    require_once 'models/cocktail.php';
+    require_once './lib/database.php';
+    require_once './models/cocktail.php';
 
-    //connexion a la bdd
     connexionMySQL();
-    
-    $result = getOneCocktailById($_GET['id']);
-    $errorEdit ="";
-    $errorFile = "";
-    $listFamille = getListFamille();
 
     if(
         isset($_POST['nom']) && !empty($_POST['nom']) && 
@@ -17,21 +11,31 @@
         isset($_POST['dateConception']) && !empty($_POST['dateConception']) && 
         isset($_POST['prixMoyen']) && !empty($_POST['prixMoyen']) && 
         isset($_POST['idFamille']) &&!empty($_POST['idFamille'])
-    )
-    {
-         //on test la taille du fichier pour voir si il est inferieur a 2MO
-        if($_FILES['urlPhoto']['size'] < 2000000)
-        {
+    ){
 
-            //on créer un tableau pour les type de fichier que l'on veux autoriser
+        //on teste la taille du fichier pour voir si il est inferieur à 2MO
+        if($_FILES['urlPhoto']['size'] < 2000000){
+
+            //on crée un tableau pour les types de fichier que l'on veux autoriser
             $tabTypeAuthorise = ['image/png', 'image/jpeg'];
 
-            //on test si si le type du fichier est présent dans le tableau des types autorisées
-            if(in_array($_FILES['urlPhoto']['type'], $tabTypeAuthorise))
-            {
+            //on teste si le type du fichier est présent dans le tableau des types autorisés
+            if(in_array($_FILES['urlPhoto']['type'], $tabTypeAuthorise)){
+
+                $result = getOneCocktailById($_GET['id']);
+
+                //vérification de la présence de la photo sur le serveur
+                if(is_file('www/images/cocktails/'.$result['urlPhoto'])){
+                    //suppression de la photo
+                    unlink('www/images/cocktails/'.$result['urlPhoto']);
+                }
+
+                //génére un nombre aleatoire
                 $namePhoto = uniqid();
 
-               
+                //upload la photo sur le serveur (1 origine - 1 destination)
+                move_uploaded_file($_FILES['urlPhoto']['tmp_name'], 'www/images/cocktails/'.$namePhoto);
+
                 $nom = $_POST['nom'];
                 $description = $_POST['description'];
                 $dateConception = $_POST['dateConception'];
@@ -39,27 +43,38 @@
                 $idFamille = $_POST['idFamille'];
                 $urlPhoto = $namePhoto;
 
-                //upload la photo sur le serveur (1 origine - 1 destination)
-                 move_uploaded_file($_FILES['urlPhoto']['tmp_name'], 'www/images/cocktails/'.$namePhoto);
-       
-        
-                editCocktail($nom, $description, $dateConception, $prixMoyen, $idFamille,$urlPhoto, $_GET['id']);
+                //appel de la fonction qui va modifier dans la bdd
+                modifierCocktail($_GET['id'], $nom, $description, $dateConception, $prixMoyen, $idFamille, $urlPhoto);
 
-       
+                //suppression des anciennes relations
+                deleteIngredientByCocktail($_GET['id']);
+
+                //boucle sur les ingredients pour faire autant de INSERT INTO qu'il y a d'ingredients cochés
+                foreach($_POST['ingredient'] as $idIngredient){
+                    ajouterCocktailIngredient($_GET['id'], $idIngredient);
+                }
+
+                //redirection
                 header("Location:details_cocktail.php?id=".$_GET['id']);
-            }
-            else
-            {
-                $errorEdit = "Vous n'avez pas rempli tous les champs";
+
             }
         }
-    }     
+    }
 
+    $result = getOneCocktailById($_GET['id']);
 
-    
+    //chargement des données pour l'affichage du formulaire
+    $listFamille = getListFamille();
+    $listAllIngredients = getAllIngredients();
 
+    //chargement des ingredients du cocktail
+    $listIngredientCocktail = getIngredientByCocktail($_GET['id']);
+    //création d'un tableau vide pour l'utiliser dans le in_array (dans le phtml)
+    $tabIdCocktail = array();
+    //boucle sur les ingredient du coctail
+    foreach($listIngredientCocktail as $row){
+        //ajout des id uniquement dans le tableau
+        array_push($tabIdCocktail, $row['id']);
+    }
 
-
-
-
-include 'templates/edition_cocktail.phtml';
+    include './templates/edition_cocktail.phtml';
